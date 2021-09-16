839215bbd0b474abb5efab9d835240d7e0a6f3c6
Igor Polousov <igorpolousov@gmail.com>
2021-09-16 11:53:07 +0300

    + basic functions of Project 10 to Project 12

---
+++ Project12b/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project12b
+//
+//  Created by Igor Polousov on 15.09.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+   
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
